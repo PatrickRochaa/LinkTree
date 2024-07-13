ed572d8470136acc93f863b22b64d477dafcdf4d
Patrick Rocha <patrick.rochapereira@hotmail.com>
2024-07-13 17:20:39 -0300

    atualizaçao

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -15,9 +15,9 @@ export function Header() {
     <header className="w-full max-w-2xl mt-4 px-1">
       <nav className="w-full bg-white h-12 flex items-center justify-between rounded-md px-3">
         <div className="flex gap-4 font-medium">
-          <Link to={"/Links"}>Links</Link>
-          <Link to={"/admin"}>Admin</Link>
-          <Link to={"/name"}>Nome</Link>
+          <Link to={"/Links"}>Links Criados</Link>
+          <Link to={"/admin"}>Criar Link</Link>
+          <Link to={"/name"}>Criar Nome</Link>
         </div>
 
         <button onClick={handleLogout}>
